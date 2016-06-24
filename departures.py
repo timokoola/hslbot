77fd2d3cb038f5413f980a10748d7a90780c831c
Timo Koola <timo.koola@zyptonite.com>
2016-06-25 00:07:43 +0300

    relative time for alexa

--- departures.py
+++ departures.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+import datetime
+
 import requests
 from collections import defaultdict
 
@@ -28,6 +30,21 @@ def hsl_time_to_time(x):
     return "%02d.%02d" % (x / 100 % 24, x % 100)
 
 
+def relative_minutes(stoptime, comparison_time=None):
+    if (comparison_time):
+        usertime = comparison_time
+    else:
+        usertime = datetime.datetime.now()
+    sth = stoptime / 100
+    if sth >= 24 and usertime.hour < 12:
+        nowagg = (usertime.hour + 24) * 60 + usertime.minute
+    else:
+        nowagg = usertime.hour * 60 + usertime.minute
+    stm = stoptime % 100
+    stoptimeagg = sth * 60 + stm
+    return "in %d minutes" % (stoptimeagg - nowagg)
+
+
 class HslRequests(object):
     def __init__(self, user, password):
         self.urls = HslUrls(user, password)
@@ -39,7 +56,7 @@ class HslRequests(object):
         if l:
             lines = dict([(x["code"], "%s %s" % (x["code_short"], x["line_end"])) for x in l])
         else:
-            return "Koodilla %s ei löydy pysäkkiä." % stop_code
+            return "Helsinki area has no such stop." % stop_code
 
         stop_line = s["code_short"] + " " + s["name_fi"] + " " + s["address_fi"]
 
@@ -48,6 +65,24 @@ class HslRequests(object):
                 ["%s %s" % (hsl_time_to_time(x["time"]), lines[x["code"]]) for x in s["departures"][:3]])
         else:
             departure_line = ""
+
+        return "\n".join([stop_line, departure_line])
+
+    def relative_time(self, stop_code):
+        (stop_info, l) = self._stop_info_lines_info(stop_code)
+        s = stop_info[0]
+        if l:
+            lines = dict([(x["code"], "%s" % (x["code_short"])) for x in l])
+        else:
+            return "Helsinki area has no such stop." % stop_code
+
+        stop_line = "Stop %s" % s["code_short"]
+
+        if s["departures"]:
+            departure_line = "\n".join(
+                ["%s %s" % (lines[x["code"]], relative_minutes(x["time"])) for x in s["departures"][:3]])
+        else:
+            departure_line = "No departures within next 60 minutes"
 
         return "\n".join([stop_line, departure_line])
 
@@ -89,7 +124,7 @@ class HslRequests(object):
         if l:
             linecodes = dict([(x["code"], x["code_short"]) for x in l])
         else:
-            return "Koodilla %s ei löydy pysäkkiä." % stop_code
+            return "Helsinki area has no such bus stop" % stop_code
 
         d = dict(map(lambda x: x.split(":"), s["lines"]))
 
@@ -117,6 +152,6 @@ class HslRequests(object):
         s = self._location_stops(longitude, latitude)
 
         if s == "Error":
-            return "Ei pysäkkejä tässä sijainnissa"
+            return "No stops nearby this location"
 
         return "\n".join(["%s %s %s" % (x["codeShort"], x["name"], x["address"]) for x in s])
