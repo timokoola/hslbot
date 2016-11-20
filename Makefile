9229a6f6177c0271d20fd889a6ee02a3b27e399f
Timo Koola <timo.koola@zyptonite.com>
2016-11-20 14:50:53 +0200

    basics of messenger bot

--- Makefile
+++ Makefile
@@ -6,10 +6,12 @@ deploy: clean package staging-upload upload
 
 telegramdeploy: clean telegrampackage telegramupload
 
+facebookdeploy: clean facebookpackage messengerupload
+
 staging: clean package staging-upload integrationtest
 
 clean:
-	rm -rf hslbot.zip rm -rf telegrambot.zip
+	rm -rf hslbot.zip rm -rf telegrambot.zip -rf messengerbot.zip
 
 package:
 	zip -r hslbot.zip departures.py lambdamain.py alexarequests.py keys.py requests pytz
@@ -17,8 +19,14 @@ package:
 telegrampackage:
 	zip -r telegrambot.zip departures.py telegramlambda.py keys.py requests telepot urllib3 pytz
 
+facebookpackage:
+	zip -r messengerbot.zip departures.py messengerlambda.py keys.py pymessenger pytz requests urllib3 six requests_toolbelt
+
 upload:
 	aws lambda update-function-code --function-name $(LAMBDANAME) --zip-file fileb://hslbot.zip --region us-east-1
+
+messengerupload:
+	aws lambda update-function-code --function-name hslmessengerbot --zip-file fileb://messengerbot.zip --region us-east-1
 
 staging-upload:
 	aws lambda update-function-code --function-name hslstop --zip-file fileb://hslbot.zip --region eu-west-1
---
+++ messengerlambda.py
@@ -0,0 +1,80 @@
+"""Lambda handle function for messenger bot"""
+from __future__ import print_function
+
+import re
+
+import departures
+from departures import normalize_stopcode
+from keys import hsl_username, hsl_passcode, facebook_bot_key
+from pymessenger.bot import Bot
+import json
+
+
+def lambda_handler(event, context):
+    """Lambda handle function for messenger bot, calls are
+    routed through API gateway"""
+    print(json.dumps(event))
+    hsl = departures.HslRequests(hsl_username,
+                                 hsl_passcode)
+    #bot = telepot.Bot(telegram_api_key)
+    #on_chat_message(event["message"], hsl, bot)
+    try:
+        return int(event["params"]["querystring"]["hub.challenge"])
+    except KeyError:
+        bot = Bot(facebook_bot_key)
+        recipient_id = event["body-json"]["entry"][0]["messaging"][0]["sender"]["id"]
+        bot.send_text_message(recipient_id, send_help_text(None))
+
+
+
+def send_help_text(bot):
+    """Help text"""
+    return "Hello, this bot offers Helsinki area public transportation info.\n" \
+           "Send a location to bot to get stops near you.\n" \
+           "Send a stop code to see departures\n" \
+           "For example try 'V0531' for airport trains to Helsinki\n" \
+           "or '0099' for Suomenlinna ferries\n" \
+           "search for stops with 'search address'\n" \
+           "and get stop info with 'info stopcode'\n"
+
+
+def on_chat_message(msg, hsl, bot):
+    content_type, chat_type, chat_id = telepot.glance(msg)
+    if content_type == u"location":
+        location = msg["location"]
+        latitude = location["latitude"]
+        longitude = location["longitude"]
+        reply, stops = hsl.stops_for_location(latitude, longitude)
+        bot.sendMessage(chat_id, reply)
+    if content_type == u"text":
+        text = msg["text"]
+        if text.find("help") != -1:
+            bot.sendMessage(chat_id, send_help_text(bot))
+            return
+        elif text.lower().startswith("search"):
+            term = " ".join(text.lower().split(" ")[1:])
+            card = hsl.stops_for_query(term)[0]
+            bot.sendMessage(chat_id, card, "Markdown")
+            return
+        elif text.lower().startswith("info"):
+            term = "".join(text.split(" ")[1])
+            card = hsl.stop_lines_summary(term)
+            bot.sendMessage(chat_id, card, "Markdown")
+            return
+        m = re.match(r".*\b(\D+)(\d+)", text)
+        if m:
+            card = get_stop_text(m.groups()[0], m.groups()[1], hsl)
+            bot.sendMessage(chat_id, card)
+        else:
+            m = re.match(r".*\b(\d+)", text)
+            if m:
+                card = get_stop_text("", m.groups()[0], hsl)
+                bot.sendMessage(chat_id, card, "Markdown")
+
+    print('Chat Message:', content_type, chat_type, chat_id)
+
+
+def get_stop_text(city, stop_code, hsl):
+    normalized = normalize_stopcode(stop_code)
+    _, card, actual = hsl.relative_time(city + normalized, 6, "*")
+    return "*%s %s" % (actual, card)
